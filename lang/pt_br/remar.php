345aa4ba4c531f46c6cbf349f5551772f60c7c9e
Rener Baffa da Silva <renerbaffa@gmail.com>
2015-04-02 08:53:19 -0300

    Adicionando arquivo para língua em português

---
+++ lang/pt_br/remar.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'remar';
+$string['modulenameplural'] = 'remars';
+$string['modulename_help'] = 'Descrição do remar na listagem de atividades';
+$string['remarfieldset'] = 'Campo customizado de exemplo';
+$string['remarname'] = 'REMAR';
+$string['remarname_help'] = 'Este é o conteúdo do tooltip de ajuda.';
+$string['remar'] = 'remar';
+$string['pluginadministration'] = 'remar administration';
+$string['pluginname'] = 'remar';
